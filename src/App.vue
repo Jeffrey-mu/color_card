288b16d99e8e13fe303704e8cfa8f2dce8f76b27
Jeffrey <1115378579@qq.com>
2022-08-19 09:32:45 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="app">
+    <div class="header">
+      Welcome to the color card and see if there is any color you like.
+    </div>
+    <div class="content">
+      <ColorCard
+        v-for="[left, right] in colorList"
+        :left="left"
+        :right="right"
+        :key="left"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup name="IndexPage">
+import { computed, ref } from "vue";
+import ColorCard from "./components/ColorCard.vue";
+function getColor() {
+  var str = "#";
+  var arr = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+  for (var i = 0; i < 6; i++) {
+    var num = parseInt(Math.random() * 16 + "");
+    str += arr[num];
+  }
+  return str;
+}
+const count = ref(100);
+const colorList = computed(() => {
+  return new Array(+count.value)
+    .fill(new Array(2).fill(null))
+    .map((el) => el.map((el) => (el = getColor())));
+});
+</script>
+<style scoped>
+.input {
+  width: 150px;
+  background: none;
+}
+.header {
+  font-size: 22px;
+  text-align: center;
+  color: rgb(170, 12, 165);
+  line-height: 50px;
+}
+.app {
+  width: 100%;
+  height: 100vh;
+  background: conic-gradient(
+    from 192deg at 46.5% 42.58%,
+    #fa8792 -54.94deg,
+    #561bbe 28.29deg,
+    #70e6fb 157.82deg,
+    #fbf8b3 220.83deg,
+    #fa8792 305.06deg,
+    #561bbe 388.29deg
+  );
+}
+.content {
+  overflow: auto;
+  width: 100%;
+  height: calc(100vh - 50px);
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
---
+++ src/components/ColorCard.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const porps = defineProps<{ left: string; right: string }>();
+const left = ref({
+  background: porps.left,
+  boxShadow: "1px 1px 1px 1px #111",
+});
+const right = ref({
+  background: porps.right,
+  boxShadow: "1px 1px 10px 1px #111",
+});
+</script>
+<template>
+  <div class="card">
+    <div class="left float" :style="left">
+      <div class="in_left in" :style="left">
+        {{ left.background }}
+      </div>
+    </div>
+    <div class="right float" :style="right">
+      <div class="in_right in" :style="right">
+        {{ right.background }}
+      </div>
+    </div>
+    <!--
+     -->
+  </div>
+</template>
+<style scoped>
+.card {
+  margin: 10px;
+  width: 300px;
+  height: 400px;
+  border-radius: 20px;
+  display: flex;
+  overflow: hidden;
+  position: relative;
+}
+.right {
+}
+.float {
+  /* float: left; */
+  width: 150px;
+  height: 100%;
+}
+.left {
+}
+.in {
+  margin: 0 auto;
+  line-height: 90px;
+  text-align: center;
+  font-size: 20px;
+  z-index: 2;
+  color: #fff;
+  border-radius: 20px;
+  box-sizing: border-box;
+  transition: all 0.2s;
+}
+.in:hover {
+  transform: scale(1.02);
+  transition: all 0.2s;
+}
+.in_right {
+  width: 260px;
+  height: 170px;
+  position: absolute;
+  top: 210px;
+  left: -130px;
+}
+.in_left {
+  width: 260px;
+  height: 170px;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+}
+</style>
